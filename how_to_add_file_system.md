16129afbbba9d440b54d3e61b03ba0fc9da3defa
Brandon Tarney <brandon.tarney@gmail.com>
2017-05-04 23:26:48 -0400

    3/4 questions on HW answered, added hw4_fs for reference

--- how_to_add_file_system.md
+++ how_to_add_file_system.md
@@ -5,9 +5,13 @@
 - Project 4 Part 2 - Adding a new file system type to Minix
 
 ## How to create a new filesystem type 
+New file systems are created with the `mkfs` command meaning we will need a new implementation of this command. The file system is identical to v3 Minix filesystem, aside from the file system type which is embedded in the superblock of the file system when it is created. First we would need to update the makefile for the appropriate program (i.e. PROG= mkfs.mfshw4). Next we modify the magic # (essentially the file system type) contained in the super block which can be found in sbin.mkfs/v3/const.h (#define SUPER_V3 0x4d5a) as well as the macro SUPER_MAGIC which points to SUPER_V# (we simply point to our renamed SUPER_V3 which now has a new macro). This "magic number" is stored in Super.H struct super_block superblock.s_magic and is used to precalculate s_inodes_per_block. 
+
 
 ## How to create a disk with that new filesystem
+"MKFS" is the standard unix utility which creates a file system on a fresh disk. It's typcially located on /sbin/. The exact use of this utility would look something like "mkfs.hw4 -t <type> <partition>" . We will use the type we just created and any partition available.
 
 ## How to create a new filesystem service associated with the new filesystem
+I should probalby have mentioned by now we should rename all the #ifndef & @define statements which use "MFS" and replaced them with "fs_h4" or something like it. There are many constants and variables we should probably rename. We might reconsider some macro's as well, such as mfsdir.h MFS_DIRSIZ which controls the directory size (name) We would have to modify the makefile first and foremost to build our service (fs_hw4, instead of mfs). Once again we must modify const.h to include a macro defintion of our file system type and its associated magic number. We can also modify Super.c just to add the new file system to show our file system only supports our new file system type. 
 
 ## How to modify the 'mount' command to mount the new filesystem within the Minix file system hierarchy
---
+++ hw4_fs/time.d
@@ -0,0 +1,35 @@
+time.o: time.c fs.h /usr/include/minix/config.h \
+  /usr/include/minix/sys_config.h /usr/include/sys/types.h \
+  /usr/include/sys/featuretest.h /usr/include/machine/types.h \
+  /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
+  /usr/include/sys/cdefs_elf.h /usr/include/machine/int_types.h \
+  /usr/include/machine/ansi.h /usr/include/sys/ansi.h \
+  /usr/include/machine/endian.h /usr/include/sys/endian.h \
+  /usr/include/machine/endian_machdep.h /usr/include/machine/bswap.h \
+  /usr/include/machine/byte_swap.h /usr/include/sys/bswap.h \
+  /usr/include/sys/fd_set.h /usr/include/minix/const.h \
+  /usr/include/machine/archconst.h /usr/include/sys/null.h \
+  /usr/include/minix/type.h /usr/include/machine/multiboot.h \
+  /usr/include/sys/sigtypes.h \
+  /usr/pkg/bin/../lib/clang/3.4/include/stdint.h /usr/include/stdint.h \
+  /usr/include/machine/int_mwgwtypes.h /usr/include/machine/int_limits.h \
+  /usr/include/machine/int_const.h /usr/include/machine/wchar_limits.h \
+  /usr/include/machine/interrupt.h /usr/include/minix/dmap.h \
+  /usr/include/minix/ipc.h /usr/include/minix/ipcconst.h \
+  /usr/include/machine/ipcconst.h /usr/include/sys/signal.h \
+  /usr/include/sys/siginfo.h /usr/include/machine/signal.h \
+  /usr/include/machine/fpu.h /usr/include/machine/trap.h \
+  /usr/include/x86/trap.h /usr/include/sys/ucontext.h \
+  /usr/include/machine/mcontext.h /usr/include/lib.h \
+  /usr/pkg/bin/../lib/clang/3.4/include/limits.h /usr/include/limits.h \
+  /usr/include/machine/limits.h /usr/include/sys/syslimits.h \
+  /usr/include/errno.h /usr/include/sys/errno.h /usr/include/minix/com.h \
+  /usr/include/minix/callnr.h /usr/include/minix/endpoint.h \
+  /usr/include/minix/vfsif.h /usr/include/minix/syslib.h \
+  /usr/include/minix/u64.h /usr/include/minix/devio.h \
+  /usr/include/minix/safecopies.h /usr/include/minix/vm.h \
+  /usr/include/minix/sef.h /usr/include/stdio.h \
+  /usr/include/minix/sysutil.h mfsdir.h const.h type.h \
+  /usr/include/minix/libminixfs.h proto.h glo.h inode.h \
+  /usr/include/sys/queue.h super.h /usr/include/sys/stat.h \
+  /usr/include/sys/time.h /usr/include/sys/select.h /usr/include/time.h
